5cca168f64cb43b1846aed29b27ac410c4988180
NICOLE\Nicole <annicole_rosas@outlook.com>
2022-07-19 14:35:15 -0500

    new routes, delete, put , finone

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -8,7 +8,35 @@ export const finAllTask = async (req,res)=>{
 
 export const createTask = async (req,res)=>{
     console.log(req.body)
-    const newTask= new Task({title:req.body.title,description:req.body.description})
+    const newTask= new Task({
+        title:req.body.title,
+        description:req.body.description,
+        done: req.body.done ? req.body.done : false 
+    })
     const taskSaved= await newTask.save();
     res.json(taskSaved)
 }
+
+export const findOneTask = async (req,res)=>{
+    console.log(req.params.id)
+    const task= await Task.findById(req.params.id);
+    res.json(task)
+};
+
+export const findAlldoneTask = async (req,res)=>{
+    const tasks= await Task.find({done:true});
+
+    res.send(tasks)
+};
+
+export const deleteTask = async (req,res)=>{
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({
+        message: 'Task were deleted'
+    });
+};
+
+export const updateTask= async (req,res)=>{
+   const updated= await Task.findByIdAndUpdate(req.params.id,req.body);
+   res.json({message:"Task was updated"});
+};
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -7,5 +7,13 @@ const router= Router();
 router.get('/tasks',taskcontroller.finAllTask);
 
 router.post('/tasks',taskcontroller.createTask)
+router.get('/tasks/done',taskcontroller.findAlldoneTask);
+
+router.get('/tasks/:id',taskcontroller.findOneTask);
+
+router.delete('/tasks/:id',taskcontroller.deleteTask);
+
+router.put('/tasks/:id',taskcontroller.updateTask);
+
 
 export default router;
